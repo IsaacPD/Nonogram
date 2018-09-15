3dc939969df336883d0edd74fb7bb9c28b8a6d00
Isaac <isaacpd@yahoo.com>
2018-09-15 02:50:31 -0400

    Added column and row hints

--- game.py
+++ game.py
@@ -27,6 +27,16 @@ class BitVector2D:
 			mask = ~mask
 			self.vector &= mask
 	
+	def __str__(self):
+		result = ''
+		copy = self.vector
+		for _ in range(self.r):
+			for _ in range(self.c):
+				result += str(copy & 1) + ' '
+				copy = copy >> 1
+			result += '\n'
+		return result
+
 	def __eq__(self, other):
 		if not isinstance(other, BitVector2D):
 			return False
@@ -47,35 +57,75 @@ class Game:
 		self.stop = Event()
 		self.clock = ClockTimer(self.stop)
 
+		self.won = True
 		self.image = image
 		self.rows, self.cols = image.size
+
+		self.rhints = [[] for _ in range(self.rows)]
+		self.chints = [[] for _ in range(self.cols)]
+
 		self.board = BitVector2D(self.cols, self.rows)
 		self.pboard = BitVector2D(self.cols, self.rows)
+		self.guesses = dict()
+
 		self.init()
 
 	def init(self):
 		pixels = self.image.getdata()
 		row = 0
 		col = 0
-		for r,g,b in pixels:
-			print(r,g,b)
+
+		rhi  = 0
+		chi = [0 for _ in range(self.cols)]
+
+		for r,g,b in pixels: 
 			if r + g + b == BLACK:
+				rhi += 1
+				chi[col] += 1
 				self.board.set(row, col, BLACK)
 			else:
 				self.board.set(row, col, WHITE)
-			col += 1
+				if chi[col] != 0:
+					self.chints[col].insert(0, chi[col])
+					chi[col] = 0
+				if rhi != 0:
+					self.rhints[row].insert(0, rhi)
+					rhi = 0
 
-			if col > self.cols:
+			col += 1
+			if col >= self.cols:
+				if rhi != 0:
+					self.rhints[row].insert(0, rhi)
+					rhi = 0
 				row += 1
 				col = 0
+		for i in range(len(chi)):
+			if chi[i] != 0:
+				self.chints[i].insert(0, chi[i])
 	
 	def make_guess(self, row, col, val):
 		actual = self.board.get(row, col)
+
+		self.guesses[(row, col)] = (actual == val)
 		self.pboard.set(row, col, val)
+
+		if self.win():
+			self.stop.set()
+			print(self.clock.timer)
+			print("You Win")
+			game.won = True
 
 	def win(self):
 		return self.board == self.pboard
+	
+	def __str__(self):
+		return str(self.pboard)
 
-img = Image.open('test.png', 'r')
-game = Game(img)
-l = len(bin(game.board.vector)) - 2
+if __name__ == '__main__':
+	img = Image.open('test.png', 'r')
+	game = Game(img)
+	print(game.chints)
+	while not game.won:
+		print(game)
+		guess = [int(x) for x in input('Guess: ').split(' ')]
+		game.make_guess(guess[0], guess[1], guess[2])
