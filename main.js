6916b99bed86fa8d0e2ff17d43a43ffb51c2b194
limboaz <limboaz@outlook.com>
2018-09-15 15:41:18 -0400

    Initialized Page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 let win
 
 function createWindow() { 
-   win  = new BrowserWindow({ frame: false, width: 640, height: 480 })  
+   win  = new BrowserWindow({ frame: false, width: 640, height: 480, backgroundColor: '#305426'})  
    win.loadFile('./src/index.html')
 }  
 
